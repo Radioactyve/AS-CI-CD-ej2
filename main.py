f10276ec3e16f03c55fdef5400a3747d54497bfd
Radioactyve <128220993+Radioactyve@users.noreply.github.com>
2024-11-12 16:13:06 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+# main.py
+
+def sumar(a, b):
+    """Función que suma dos números."""
+    return a + b
+
+# Dos variables con números a elección
+num1 = 5
+num2 = 10
+
+# Imprime la suma de los dos números
+print("La suma es:", sumar(num1, num2))
